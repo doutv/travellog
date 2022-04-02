a23b747da8e1099314521205a7c9479fed6c934a
Yongjin Huang <1765941460@qq.com>
2022-04-02 17:01:41 +0800

    update mapbox version and add map elements

--- local_test.sh
+++ local_test.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
-
+python3 -m pip install -r requirements.txt
 set -a
 source test.env
 set +a
-# docker run --name travellog-test --env-file .env -p3306:3306 -d mysql
+docker run --name travellog-test --env-file test.env -p3306:3306 -d mysql
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
