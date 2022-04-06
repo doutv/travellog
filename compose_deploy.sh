8cc7dd07714a2fdca556c3b5b13bcdd6159a340f
Yongjin Huang <1765941460@qq.com>
2022-04-06 14:53:39 +0800

    add deploy scripts

--- compose_deploy.sh
+++ compose_deploy.sh
@@ -2,10 +2,10 @@
 set -a
 source deploy.env
 set +a
-python3 manage.py generate &&\
+docker-compose exec backend bash -c "python3 manage.py generate" &&\
 mkdir -p .deploy &&\
 cd .deploy &&\
 git init &&\
-git add -A &&\
+sudo git add -A &&\
 git commit -m "update deployment" &&\
 git push -u $DEPLOY_GIT_URL HEAD:$DEPLOY_BRANCH --force
--- local_test.sh
+++ local_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 python3 -m pip install -r requirements.txt
 set -a
-source test.env
+source deploy.env
+export MYSQL_HOST=127.0.0.1
 set +a
-docker run --name travellog-test-qianniu --env-file test.env -p3306:3306 -d mysql
+docker run --name travellog-test-mysql --env-file deploy.env -p3306:3306 -d mysql
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput
