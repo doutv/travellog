996c4efa0a748ad7475c97b07b5466dec49f43a5
Yongjin Huang <1765941460@qq.com>
2022-04-06 13:41:17 +0800

    testing~

--- local_deploy.sh
+++ local_deploy.sh
@@ -2,10 +2,10 @@
 set -a
 source test.env
 set +a
-python3 manage.py generate
-git checkout deploy
-git checkout master index.html
-git add index.html
-git commit -m "Updated index.html"
-git checkout master
-rm index.html
+python3 manage.py generate &&\
+git checkout deploy &&\
+git checkout master index.html &&\
+git add index.html &&\
+git commit -m "Updated index.html" &&\
+git checkout master &&\
+git checkout .
